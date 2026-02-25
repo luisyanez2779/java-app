56ab63f4ed3ece54cbab369f0c6716a3926f244d
Luis Yanez <luisyanez2779@gmail.com>
2026-02-24 22:13:49 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,12 +4,13 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "building application"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+      sh 'docker build -t luisyanez27/demo-app:jma-2.0 .'
+      sh 'echo $PASS | docker login -u $USER --password-stdin'
+      sh 'docker push luisyanez27/demo-app:jma-2.0'
     }
+    
 } 
 
 def deployApp() {
