ba88ff8c5ed77d612b314ae34a7cc3d453d3f71c
Luis Yanez <luisyanez2779@gmail.com>
2026-02-26 12:30:47 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,24 +1,23 @@
 def testApp() {
-    echo 'deploying the application for branch $BRANCH_NAME...'
+    echo "testing the application for ${BRANCH_NAME}..."
 } 
 
 def buildJar() {
-    echo "building the application..."
+    echo "building the application for ${BRANCH_NAME}..."
     sh 'mvn package'
 } 
 
 def buildImage() {
-    echo "building application"
+    echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-      sh 'docker build -t luisyanez27/demo-app:jma-2.0 .'
-      sh 'echo $PASS | docker login -u $USER --password-stdin'
-      sh 'docker push luisyanez27/demo-app:jma-2.0'
+        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push nanajanashia/demo-app:jma-2.0'
     }
-    
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo "deploying the application for ${BRANCH_NAME}..."
 } 
 
 return this
